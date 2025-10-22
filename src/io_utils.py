d514e66f45717d78145b3f5a3055e111d5f379d3
Victoria Elmander <victoria.elmander@gmail.com>
2025-10-22 14:37:20 +0200

    csv_read-funktion i io_utils, samt paths.py, har anropat från report.ipynb.

--- src/io_utils.py
+++ src/io_utils.py
@@ -1,10 +1,26 @@
 import csv
-from paths import ECOMMERCE_FILE
+from .paths import ECOMMERCE_FILE
 
-with open(ECOMMERCE_FILE, "r", encoding="utf-8") as file:
-    reader = csv.DictReader(file, skipinitialspace=True) #Läser första raden som nycklar
-    header = reader.fieldnames
-
-    print(header)
+def read_csv(file_path=ECOMMERCE_FILE) :
+    #Returnerar hela CSV-filen som list[dict]
+    
+    with open(file_path, "r", encoding="utf-8") as file:
+        reader = csv.DictReader(file, skipinitialspace=True) #Läser första raden som nycklar
+        rows = list(reader)
+        header = reader.fieldnames
+    return header, rows
     # for row in reader:
     #     print(row["city"])
+if __name__ == "__main__":
+    print("Testar read_csv...\n")
+
+    header, rows = read_csv()
+    print("Header:", header)
+    print("Antal rader:", len(rows))
+
+    if rows:
+        print("\nFörsta rad:")
+        print(rows[0])
+    else:
+        print("Inga rader hittades!")
+
