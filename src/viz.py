75e1629ea48f63cad49384feb26a60b60ec5bffd
Ralle <Rasmus.svensson96@gmail.com>
2025-10-31 12:54:06 +0100

    added average_price_by_category to metrics, imported viz def to class & plotted it in report to test, not saved image yet.

--- src/viz.py
+++ src/viz.py
@@ -36,6 +36,7 @@ def plt_revenue_by_category(df):
 
 def plot_revenue_over_time(df):
     monthly_revenue = revenue_over_time(df, freq='ME')
+    monthly_revenue.index = monthly_revenue.index.strftime("%b %Y")
 
     plt.figure(figsize=(10, 6))
     plt.plot(monthly_revenue.index.astype(str), monthly_revenue.values, marker='o', linestyle='-')
@@ -43,6 +44,6 @@ def plot_revenue_over_time(df):
     plt.xlabel("Månad")
     plt.ylabel("Intäkt (kr)")
     plt.xticks(rotation=45)
-    plt.grid(True)
+    plt.grid(True, linestyle="--", alpha=0.6)
     plt.tight_layout()
     plt.show()
